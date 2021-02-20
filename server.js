1c2d8c8b7ef1fbe172c9f3af05b6d8cc0137f72d
Relten98 <diezhandinoty@gmail.com>
2021-02-20 11:22:42 -0800

    Update server.js

--- server.js
+++ server.js
@@ -1,14 +1,20 @@
 // Dependencies / Important stuff
 const express = require('express');
-const birds = 15000
 const app = express();
 
+// Important for future bird counting reference.
+const birds = 15000
+
+// Port
 const PORT = process.env.PORT || 8080;
+
+// Other server stuff will go below this line.
+// ///////////////////////////////////////////
+
 
 
 // The magic that stats this whole thing
 app.listen(PORT, function () {
-    console.log(`Woo Yeah Baby! That's What I've Been Waiting For!`);
     console.log(`Application is listening on port: ${PORT}`);
     console.log(`And there are currenly ${birds} birds flying in the sky.`);
 });
