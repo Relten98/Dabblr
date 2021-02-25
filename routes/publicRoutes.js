8481902048ec201145f0826834f0e8a1c5335c8a
C.T. Bell <charlestbell@gmail.com>
2021-02-24 21:47:55 -0500

    Refactoring and moved the topic query to the topic model under the name 'getTopic'

--- routes/publicRoutes.js
+++ routes/publicRoutes.js
@@ -1,34 +1,34 @@
 // Import our requirements
-const db = require('../models');
-const puppeteer = require('puppeteer');
+const db = require('../models')
+const puppeteer = require('puppeteer')
 
 // const Article = require('./components/Article');
 
 module.exports = (app) => {
     app.get('/', async (req, res) => {
-        const browser = await puppeteer.launch();
-        const page = await browser.newPage();
+        const browser = await puppeteer.launch()
+        const page = await browser.newPage()
 
         // Mamamia papapia, we ain't gettin no wiki-ria
         // let wiki = new Article("https://en.wikipedia.org/wiki/Footwear", 32, browser, page);
         // let [header, summary] = await wiki.getInfo(true);
 
-        db.topic.findOne({ where: { id: 1 } }).then((dbTopic) => {
-            console.log("topic: ", dbTopic);
+        await db.topic.getTopic(1).then((dbTopic) => {
+            console.log('topic: ', dbTopic)
 
-            let hbData = {
-                href: wiki.href,
+            const hbData = {
+                // href: wiki.href,
                 header: dbTopic.topicName,
-                score: "+9001",
+                score: '+9001',
                 // score: wiki.score,
-                summary: "the cake is a lie",
+                summary: 'the cake is a lie',
                 // source: wiki.getSource()
-            };
+            }
 
             // The information belowe will feed into the handlebar renderer
 
             // Handlebar renderer
-            res.render('index', hbData);
+            res.render('index', hbData)
         })
     })
 }
