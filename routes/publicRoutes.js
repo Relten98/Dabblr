3bd49932813efa87b18a96a2976211674ee53b70
Ankush <ankushchalla@gmail.com>
2021-02-25 15:15:02 -0800

    Getting hb and puppeteer to work together.

--- routes/publicRoutes.js
+++ routes/publicRoutes.js
@@ -2,34 +2,88 @@
 const db = require('../models');
 const puppeteer = require('puppeteer');
 
-// const Article = require('./components/Article');
+const Article = require('../components/Article');
+
+let testDB = [
+    {
+        href: "https://en.wikipedia.org/wiki/Footwear",
+        score: 32,
+    },
+    {
+        href: "https://www.dolitashoes.com/blogs/news/the-history-and-evolution-of-shoes",
+        score: 17,
+    },
+    {
+        href: "https://allthatsinteresting.com/fascinating-history-footwear",
+        score: -100,
+    }
+];
 
 module.exports = (app) => {
     app.get('/', async (req, res) => {
         const browser = await puppeteer.launch();
         const page = await browser.newPage();
 
-        // Mamamia papapia, we ain't gettin no wiki-ria
-        // let wiki = new Article("https://en.wikipedia.org/wiki/Footwear", 32, browser, page);
-        // let [header, summary] = await wiki.getInfo(true);
+        let main;
+        let altArticles = [];
+        for (let i = 0; i < testDB.length; i++) {
+            let article = new Article(testDB[i].href, testDB[i].source, browser, page);
+            let [header, summary] = ["", ""];
+            let source = article.getSource();
+            if (i === 0) {
+                [header, summary] = await article.getInfo(true);
+                main = {
+                    header: header,
+                    summary: summary,
+                    score: testDB[i].score,
+                    href: testDB[i].href,
+                    source: source
+                };
+            }
+            else {
+                [header, summary] = await article.getInfo(false);
+                altArticles.push({
+                    header: header,
+                    summary: summary,
+                    score: testDB[i].score,
+                    href: testDB[i].href,
+                    source: source
+                });
+            }
+        }
 
-        db.topic.findOne({ where: { id: 1 } }).then((dbTopic) => {
-            console.log("topic: ", dbTopic);
+        res.render('index', {
+            main: main,
+            alts: altArticles
+        });
+    });
 
-            let hbData = {
-                href: wiki.href,
-                header: dbTopic.topicName,
-                score: "+9001",
-                // score: wiki.score,
-                summary: "the cake is a lie",
-                // source: wiki.getSource()
-            };
 
-            // The information belowe will feed into the handlebar renderer
+    // app.get('/', async (req, res) => {
+    //     const browser = await puppeteer.launch();
+    //     const page = await browser.newPage();
 
-            // Handlebar renderer
-            res.render('index', hbData);
-        })
-    })
+    //     // Mamamia papapia, we ain't gettin no wiki-ria
+    //     // let wiki = new Article("https://en.wikipedia.org/wiki/Footwear", 32, browser, page);
+    //     // let [header, summary] = await wiki.getInfo(true);
+
+    //     db.topic.findOne({ where: { id: 1 } }).then((dbTopic) => {
+    //         console.log("topic: ", dbTopic);
+
+    //         let hbData = {
+    //             href: wiki.href,
+    //             header: dbTopic.topicName,
+    //             score: "+9001",
+    //             // score: wiki.score,
+    //             summary: "the cake is a lie",
+    //             // source: wiki.getSource()
+    //         };
+
+    //         // The information belowe will feed into the handlebar renderer
+
+    //         // Handlebar renderer
+    //         res.render('index', hbData);
+    //     })
+    // })
 }
 // haha code go boom
--- server.js
+++ server.js
@@ -10,10 +10,6 @@ const db = require('./models')
 const PORT = process.env.PORT || 8080;
 
 let app = express();
-
-// Routes
-require(`./routes/publicRoutes.js`)(app);
-
 // Set Handlebars as the default templating engine.
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
@@ -24,62 +20,16 @@ app.use(express.json())
 // Static directory
 app.use(express.static('public/styles'))
 
-let testDB = [
-  {
-    href: "https://en.wikipedia.org/wiki/Footwear",
-    score: 32,
-  },
-  {
-    href: "https://www.dolitashoes.com/blogs/news/the-history-and-evolution-of-shoes",
-    score: 17,
-  },
-  {
-    href: "https://allthatsinteresting.com/fascinating-history-footwear",
-    score: -100,
-  }
-];
-
-app.get('/', async (req, res) => {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-
-  let main;
-  let altArticles = [];
-  for (let i = 0; i < testDB.length; i++) {
-    let article = new Article(testDB[i].href, testDB[i].source, browser, page);
-    let [header, summary] = ["", ""];
-    let source = article.getSource();
-    if (i === 0) {
-      [header, summary] = await article.getInfo(true);
-      main = {
-        header: header,
-        summary: summary,
-        score: testDB[i].score,
-        href: testDB[i].href,
-        source: source
-      };
-    }
-    else {
-      [header, summary] = await article.getInfo(false);
-      altArticles.push({
-        header: header,
-        summary: summary,
-        score: db[i].score,
-        href: db[i].href,
-        source: source
-      });
-    }
-  }
-
-  res.render('index', {
-    main: main,
-    alts: altArticles
-  });
-})
+// Routes
+require(`./routes/publicRoutes.js`)(app);
 
 //Starts the server to begin listening
-db.sequelize
-    .sync()
-    .then(() =>
-        app.listen(PORT, () => console.log(`${birds} ducks are listening in on PORT ${PORT}`))
-    )
+// db.sequelize
+//   .sync()
+//   .then(() =>
+//     app.listen(PORT, () => console.log(`${birds} ducks are listening in on PORT ${PORT}`))
+//   )
+
+app.listen(PORT, () => {
+  console.log(`${birds} DUCKS ARE LISTENING IN ON PORT ${PORT}`);
+})
