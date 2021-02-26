0d0bf5edadded607b2ff5b929f0166e48651c614
C.T. Bell <charlestbell@gmail.com>
2021-02-25 21:48:09 -0500

    Adding home page and /variable topic functionality

--- routes/publicRoutes.js
+++ routes/publicRoutes.js
@@ -1,35 +1,59 @@
 // Import our requirements
-const db = require('../models')
-const puppeteer = require('puppeteer')
+const puppeteer = require('puppeteer');
+const db = require('../models');
 
 // const Article = require('./components/Article');
 
 module.exports = (app) => {
-    app.get('/', async (req, res) => {
-        const browser = await puppeteer.launch()
-        const page = await browser.newPage()
-
+    app.get('/topics/:topic', async (req, res) => {
+        const topicID = req.params.topic;
+        const browser = await puppeteer.launch();
+        const page = await browser.newPage();
+        console.log('req.params.topic ', req.params.topic);
         // Mamamia papapia, we ain't gettin no wiki-ria
         // let wiki = new Article("https://en.wikipedia.org/wiki/Footwear", 32, browser, page);
         // let [header, summary] = await wiki.getInfo(true);
 
-        await db.topic.getTopic(1).then((dbTopic) => {
-            console.log('topic: ', dbTopic)
+        // Model call functions
+        const getTopic = db.topic.getTopic(topicID);
+        const getTutorials = 'tutorial db call goes here';
+        const getVotes = 'vote db call goes here';
+        const getChildren = 'children topic db call goes here';
+        const getParent = 'parent topic db call goes here';
 
+        Promise.all([
+            getTopic,
+            getTutorials,
+            getVotes,
+            getChildren,
+            getParent,
+        ]).then((dbData) => {
+            const [topic, tutorials, votes, children, parent] = dbData;
+
+            // console.log('topicName ', topicName)
             const hbData = {
                 // href: wiki.href,
-                header: dbTopic.topicName,
+                header: topic.topicName,
                 score: '+9001',
                 // score: wiki.score,
                 summary: 'the cake is a lie',
                 // source: wiki.getSource()
-            }
-
+            };
             // The information belowe will feed into the handlebar renderer
-
             // Handlebar renderer
-            res.render('index', hbData)
-        })
-    })
-}
-// haha code go boom
+            res.render('index', hbData);
+        });
+    });
+
+    app.get('/', async (req, res) => {
+        const hbData = {
+            // href: wiki.href,
+            header: 'Home Page',
+            score: '+9001',
+            // score: wiki.score,
+            summary: 'the cake is a lie',
+            // source: wiki.getSource()
+        };
+        res.render('index', hbData);
+    });
+};
--- server.js
+++ server.js
@@ -1,48 +1,28 @@
 // Import our requirements
-const express = require('express')
-const exphbs = require('express-handlebars')
-const birds = 15000
+const express = require('express');
+const exphbs = require('express-handlebars');
+const birds = 15000;
 
 // Load models folder
-const db = require('./models')
+const db = require('./models');
 
 // Port information
-const PORT = process.env.PORT || 8080
+const PORT = process.env.PORT || 8080;
 
-let app = express()
+let app = express();
 
 // Routes
-require(`./routes/publicRoutes.js`)(app)
+require(`./routes/publicRoutes.js`)(app);
 
 // Set Handlebars as the default templating engine.
-app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
-app.set('view engine', 'handlebars')
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
 
 // Sets up the Express app to handle data parsing
-app.use(express.urlencoded({ extended: true }))
-app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 // Static directory
-app.use(express.static('public'))
-
-app.get('/', async (req, res) => {
-    const browser = await puppeteer.launch()
-    const page = await browser.newPage()
-    let wiki = new Article(
-        'https://en.wikipedia.org/wiki/Footwear',
-        32,
-        browser,
-        page
-    )
-    let [header, summary] = await wiki.getInfo(true)
-
-    res.render('index', {
-        href: wiki.href,
-        header: header,
-        score: wiki.score,
-        summary: summary,
-        source: wiki.getSource(),
-    })
-})
+app.use(express.static('public'));
 
 // Starts the server to begin listening
 db.sequelize
@@ -51,4 +31,4 @@ db.sequelize
         app.listen(PORT, () =>
             console.log(`${birds} ducks are listening in on PORT ${PORT}`)
         )
-    )
+    );
