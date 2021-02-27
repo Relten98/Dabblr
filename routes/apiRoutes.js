7656e019bfd9c4aa6860b854cdf3291c8262c208
C.T. Bell <charlestbell@gmail.com>
2021-02-27 12:51:27 -0500

    Refactoring error handling and success messaging on api routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -7,35 +7,42 @@ module.exports = (app) => {
         // console.log('req.body ', JSON.stringify(req.body.tutorialType));
         const userID = 1;
         res.send(req.body);
-        try {
-            db.tutorial
-                .create({
-                    tutorialType: req.body.tutorialType,
-                    tutorialLink: req.body.tutorialLink,
-                    fk_topicID: req.body.topicID,
-                    fk_userID: userID,
-                })
-                .then(() => res.status(201));
-        } catch (error) {
-            res.status(500).send('There was a problem adding to the database');
-        }
+        db.tutorial
+            .create({
+                tutorialType: req.body.tutorialType,
+                tutorialLink: req.body.tutorialLink,
+                fk_topicID: req.body.topicID,
+                fk_userID: userID,
+            })
+            .then(() =>
+                res.status(201).send('Sucessfully added tutorial to database')
+            )
+            .catch(() => {
+                res.status(500).send(
+                    'There was a problem adding to the database'
+                );
+            });
     });
 
     app.post('/api/vote', (req, res) => {
-        console.log('req.body.voteType', req.body.voteType);
-        console.log('req.body.tutorialID', req.body.tutorialID);
+        // console.log('req.body.voteType', req.body.voteType);
+        // console.log('req.body.tutorialID', req.body.tutorialID);
         const userID = 1;
-        try {
-            db.vote
-                .create({
-                    voteType: req.body.voteType,
-                    fk_tutorialID: req.body.tutorialID,
-                    fk_userID: userID,
-                })
-                .then(() => res.status(201));
-        } catch (error) {
-            res.status(500).send('There was a problem adding to the database');
-        }
+
+        db.vote
+            .create({
+                voteType: req.body.voteType,
+                fk_tutorialID: req.body.tutorialID,
+                fk_userID: userID,
+            })
+            .then(() =>
+                res.status(201).send('Successfuly added vote to database')
+            )
+            .catch(() => {
+                res.status(500).send(
+                    'There was a problem adding to the database'
+                );
+            });
         res.send();
     });
 };
