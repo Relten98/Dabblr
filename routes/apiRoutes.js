020f88c2fff4afd1ec93f43e87b94236cf365f65
Relten98 <diezhandinoty@gmail.com>
2021-02-27 14:59:03 -0800

    Fixed 'topics/topics' issue with child buttons

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,8 +3,11 @@ const db = require('../models');
 
 module.exports = (app) => {
     app.post('/api/tutorial', (req, res) => {
+
         // console.log('req ', req);
+        
         // console.log('req.body ', JSON.stringify(req.body.tutorialType));
+        
         const userID = 1;
         res.send(req.body);
         db.tutorial
